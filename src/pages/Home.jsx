64b96cf1b4d45e68f9d0cbf01201b68e306263fc
arafbinmasud <araf357300@gmail.com>
2026-03-08 12:02:44 +0600

    feat(home): add react awesome reveal to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,21 @@ import CommunityHighlights from "../components/home-page/CommunityHighlights";
 import FeaturedArts from "../components/home-page/FeaturedArts";
 import Hero from "../components/home-page/Hero";
 import TopArtists from "../components/home-page/TopArtists";
+import { Fade } from "react-awesome-reveal";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <FeaturedArts/>
-      <TopArtists />
-      <CommunityHighlights />
+      <Fade direction="up" triggerOnce={true} fraction={0.2}>
+        <FeaturedArts />
+      </Fade>
+      <Fade direction="up" triggerOnce={true} fraction={0.2}>
+        <TopArtists />
+      </Fade>
+      <Fade direction="up" triggerOnce={true} fraction={0.2}>
+        <CommunityHighlights />
+      </Fade>
     </>
   );
 };
